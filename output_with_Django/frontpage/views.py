024d007c8d282152b4f92fc1ad7e04201101bd99
winty95 <winty95@nate.com>
2018-12-04 01:52:23 +0900

    Pagination 추가, 디자인은 안된상태

--- output_with_Django/frontpage/views.py
+++ output_with_Django/frontpage/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
-
+from django.core import serializers
 from elasticsearch import Elasticsearch
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 es_client = Elasticsearch("localhost:9200")
 
 # Create your views here.
 def index(request):
+    request.session.modified = True
     if request.method == "POST":
         test = request
         # Html에서 test.POST.input_Symptom
@@ -25,36 +28,115 @@ def index(request):
                                   },
                                   size = 100
                                   )
+        print(type(result))
         count = result['hits']['total']
         disease = result['hits']['hits']
-        print(count)
-        print(disease)
+        #print(count)
+        #print(disease)
+        #print(type(count))
+        #print(type(disease))
+        paginator = Paginator(disease, 10)
+        page = request.GET.get('page')
+
+        request.session['user_input'] = user_input
+        request.session['count'] = count
+        try:
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            posts = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            posts = paginator.page(paginator.num_pages)
+
+        max_index = len(paginator.page_range)
         return render(request, 'frontpage/result.html',
-                      {'user_input': user_input, 'disease': disease, 'count': count})
+                      {'user_input': user_input, 'posts': posts, 'count': count,'max_index': max_index,})
     else:
-        return render(request, 'frontpage/index.html')
+        try:
+            del request.session['user_input']
+            del request.session['count']
+            return render(request, 'frontpage/index.html')
+        except:
+            return render(request, 'frontpage/index.html')
 
+def result(request):
+    request.session.modified = True
+    if request.method == "POST":
+        test = request
+        user_input = test.POST['input_Symptom']
+        print(user_input)
+        result = es_client.search(index='nonono',
+                                  doc_type='doc',
+                                  body={
+                                      "query": {
+                                          "multi_match": {
+                                              "query": user_input,
+                                              "analyzer": "nori",
+                                              "fields": ["diseaseko^5", "treatment", "symptom^3"]
+                                          }
+                                      }
+                                  },
+                                  size=100
+                                  )
 
-    '''    
-    elif request.method == "POST":
-        form = GallaryForm(request.POST)
-        formset = ImageFormset(request.POST or None, request.FILES or None)
-        if form.is_valid() and formset.is_valid():
-            gallary = form.save(commit=False)
-            gallary.origin_date = timezone.now()
-            gallary.final_date = timezone.now()
-            gallary.writer_name = check['name']
-            gallary.writer_id = check['id']
-            gallary.save()
+        count = result['hits']['total']
+        disease = result['hits']['hits']
 
-            for f in formset:
-                try:
-                    photo = Images(post=gallary, image=f.cleaned_data['image'])
-                    photo.save()
+        paginator = Paginator(disease, 10)
+        page = request.GET.get('page')
 
-                except Exception as e:
-                    break
-            return redirect('album')
-    return render(request, 'blog/album_edit.html',
-                  {'form': form, 'formset': formset, 'check': check})
-    '''
+        request.session['user_input'] = user_input
+        request.session['count'] = count
+        try:
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            posts = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            posts = paginator.page(paginator.num_pages)
+
+        max_index = len(paginator.page_range)
+        return render(request, 'frontpage/result.html',
+                      {'user_input': user_input, 'posts': posts, 'count': count, 'max_index': max_index, })
+
+    else:
+        try:
+            check = {'user_input': request.session['user_input']}
+        except:
+            check = False
+        if check:
+            user_input = request.session['user_input']
+            result = es_client.search(index='nonono',
+                                      doc_type='doc',
+                                      body={
+                                          "query": {
+                                              "multi_match": {
+                                                  "query": user_input,
+                                                  "analyzer": "nori",
+                                                  "fields": ["diseaseko^5", "treatment", "symptom^3"]
+                                              }
+                                          }
+                                      },
+                                      size=100
+                                      )
+            disease = result['hits']['hits']
+            paginator = Paginator(disease, 10)
+            page = request.GET.get('page')
+            try:
+                posts = paginator.page(page)
+            except PageNotAnInteger:
+                # If page is not an integer, deliver first page.
+                posts = paginator.page(1)
+            except EmptyPage:
+                # If page is out of range (e.g. 9999), deliver last page of results.
+                posts = paginator.page(paginator.num_pages)
+
+            max_index = len(paginator.page_range)
+
+            return render(request, 'frontpage/result.html',
+                          {'user_input': request.session['user_input'], 'posts': posts,
+                           'count': request.session['count'], 'max_index': max_index, })
+        else:
+            return render(request, 'frontpage/index.html')
