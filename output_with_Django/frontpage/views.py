5dfa33398585c82cb42437c85831f7be3be29b50
winty95 <winty95@nate.com>
2018-12-07 07:48:51 +0000

    Django Backend Function Optimization

--- output_with_Django/frontpage/views.py
+++ output_with_Django/frontpage/views.py
@@ -31,38 +31,39 @@ def query(user_input):
                                   size = 100)
     return result
 
+def pagenation_post(request,result):
+    count = result['hits']['total']
+    disease = result['hits']['hits']
+    paginator = Paginator(disease, 10)
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        posts = paginator.page(paginator.num_pages)
+    max_index = len(paginator.page_range)
+
+    return count,posts,max_index
+
+
 # Create your views here.
 def index(request):
     request.session.modified = True
     if request.method == "POST":
         test = request
-        # Html에서 test.POST.input_Symptom
+        # Html에서 test.POST.input_Symptom(템플릿 언어일 때)
         # python 코드에서는 접근 방법이 약간 달랐음.
         user_input = test.POST['input_Symptom']
-        print(user_input)
         result = query(user_input)
-        count = result['hits']['total']
-        disease = result['hits']['hits']
-        #print(count)
-        #print(disease)
-        #print(type(count))
-        #print(type(disease))
-
-        paginator = Paginator(disease, 10)
-        page = request.GET.get('page')
+        count,posts,max_index=pagenation_post(result)
 
         request.session['user_input'] = user_input
         request.session['count'] = count
-        try:
-            posts = paginator.page(page)
-        except PageNotAnInteger:
-            # If page is not an integer, deliver first page.
-            posts = paginator.page(1)
-        except EmptyPage:
-            # If page is out of range (e.g. 9999), deliver last page of results.
-            posts = paginator.page(paginator.num_pages)
 
-        max_index = len(paginator.page_range)
         return render(request, 'frontpage/result.html',
                       {'user_input': user_input, 'posts': posts, 'count': count,'max_index': max_index,})
     else:
@@ -80,25 +81,10 @@ def result(request):
         user_input = test.POST['input_Symptom']
         print(user_input)
         result = query(user_input)
-
-        count = result['hits']['total']
-        disease = result['hits']['hits']
-
-        paginator = Paginator(disease, 10)
-        page = request.GET.get('page')
+        count, posts, max_index = pagenation_post(request,result)
 
         request.session['user_input'] = user_input
         request.session['count'] = count
-        try:
-            posts = paginator.page(page)
-        except PageNotAnInteger:
-            # If page is not an integer, deliver first page.
-            posts = paginator.page(1)
-        except EmptyPage:
-            # If page is out of range (e.g. 9999), deliver last page of results.
-            posts = paginator.page(paginator.num_pages)
-
-        max_index = len(paginator.page_range)
         return render(request, 'frontpage/result.html',
                       {'user_input': user_input, 'posts': posts, 'count': count, 'max_index': max_index, })
 
@@ -110,23 +96,10 @@ def result(request):
         if check:
             user_input = request.session['user_input']
             result = query(user_input)
-            disease = result['hits']['hits']
-            paginator = Paginator(disease, 10)
-            page = request.GET.get('page')
-            try:
-                posts = paginator.page(page)
-            except PageNotAnInteger:
-                # If page is not an integer, deliver first page.
-                posts = paginator.page(1)
-            except EmptyPage:
-                # If page is out of range (e.g. 9999), deliver last page of results.
-                posts = paginator.page(paginator.num_pages)
+            count, posts, max_index = pagenation_post(request,result)
 
-            max_index = len(paginator.page_range)
-            # print(posts.object_list[6]['highlight']['symptom'])
-            # print(type(posts.object_list[6]['highlight']['symptom']))
-            # print(len(posts.object_list[6]['highlight']['symptom']))
-            # print(len([]))
+            request.session['user_input'] = user_input
+            request.session['count'] = count
             return render(request, 'frontpage/result.html',
                           {'user_input': request.session['user_input'], 'posts': posts,
                            'count': request.session['count'], 'max_index': max_index, })
