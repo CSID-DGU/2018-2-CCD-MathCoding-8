eaa9d450e4a9c61bd7f0c1ac39bf2d3309256210
winty95 <winty95@nate.com>
2018-12-09 22:39:47 +0900

    결과내 재검색 query 처리하는 view생성

--- output_with_Django/frontpage/urls.py
+++ output_with_Django/frontpage/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^result/$', views.result, name='result'),
+    url(r'^more_result/$', views.more_result, name='more_result'),
 ]
 urlpatterns += static('media', document_root=settings.MEDIA_ROOT)
--- output_with_Django/frontpage/views.py
+++ output_with_Django/frontpage/views.py
@@ -127,24 +127,80 @@ def result(request):
     # 결과 화면에서 POST방식 일 경우.
     ## 결과 내 재검색 체크박스가 False인 경우.
     if request.method == "POST":
-        test = request
-        user_input = test.POST['input_Symptom']
-        print(user_input)
-        result = query(user_input)
-        count, posts, max_index,current_page = pagenation_post(request,result)
+        try:
+            checkbox_content=request.POST['re-search']
+        except:
+            checkbox_content=False
+        # 결과내 재검색을 체크한 경우
+        if checkbox_content:
+            request.session['old_input']=request.session['user_input']
+            user_input = request.POST['input_Symptom']
+            result = query(request.session['old_input'],user_input)
+            count, posts, max_index, current_page = pagenation_post(request, result)
+            request.session['user_input'] = user_input
+            request.session['count'] = count
+            return render(request, 'frontpage/more_result.html',
+                          {'user_input': user_input, 'posts': posts, 'count': count,
+                           'max_index': max_index, 'current_page': current_page})
+        # 결과내 재검색을 체크하지 않은 경우
+        else:
+            user_input = request.POST['input_Symptom']
+            result = query(user_input)
+            count, posts, max_index,current_page = pagenation_post(request,result)
 
-        # print(posts.object_list[0]['highlight']['symptom'])
-        # print(posts.object_list[0]['highlight'])
-        # print(re_query('두통 복통', '설사'))
-        print(posts.object_list[0]['_source']['diseaseko'])
-        print(posts.object_list[0])
+            # print(posts.object_list[0]['highlight']['symptom'])
+            # print(posts.object_list[0]['highlight'])
+            # print(re_query('두통 복통', '설사'))
+            #print(posts.object_list[0]['_source']['diseaseko'])
+            #print(posts.object_list[0])
+
+            request.session['user_input'] = user_input
+            request.session['count'] = count
+            return render(request, 'frontpage/result.html',
+                          {'user_input': user_input, 'posts': posts, 'count': count,
+                           'max_index': max_index, 'current_page':current_page})
+    # 결과 화면에서 GET방식일 경우.
+    else:
+        try:
+            check = {'user_input': request.session['user_input']}
+        except:
+            check = False
+        # 위의 예외 처리는 Pagination이 POST방식을 활용하는 것이 아니고 GET 방식을 활용하기 때문에 나눠짐.
+        ## 결과 화면에서 사용자가 로고를 누를 때와 Pagination의 버튼을 누를 때를 나누기 위해 위의 예외처리를 사용.
+        if check:
+            user_input = request.session['user_input']
+            result = query(user_input)
+            count, posts, max_index,current_page = pagenation_post(request,result)
+
+            request.session['user_input'] = user_input
+            request.session['count'] = count
+
+            return render(request, 'frontpage/result.html',
+                          {'user_input': request.session['user_input'], 'posts': posts,
+                           'count': request.session['count'], 'max_index': max_index, 'current_page':current_page})
+        # 메인으로 돌아가기.
+        else:
+            return render(request, 'frontpage/index.html')
+
+def more_result(request):
+    request.session.modified = True
+    # 결과 화면에서 POST방식 일 경우.
+    ## 결과 내 재검색 체크박스가 False인 경우.
+    if request.method == "POST":
+        user_input = request.POST['input_Symptom']
+        result = query(user_input)
+        count, posts, max_index, current_page = pagenation_post(request, result)
+
+        #print(posts.object_list[0]['_source']['diseaseko'])
+        #print(posts.object_list[0])
 
         request.session['user_input'] = user_input
         request.session['count'] = count
+        # 추가검색을 위해 만들어둔 세션 삭제해야 함.
+        del request.session['old_input']
         return render(request, 'frontpage/result.html',
                       {'user_input': user_input, 'posts': posts, 'count': count,
-                       'max_index': max_index, 'current_page':current_page})
-    # 결과 화면에서 GET방식일 경우.
+                       'max_index': max_index, 'current_page': current_page})
     else:
         try:
             check = {'user_input': request.session['user_input']}
