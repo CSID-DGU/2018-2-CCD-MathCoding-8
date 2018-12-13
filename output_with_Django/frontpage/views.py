6f8df16f8c27249e3ccc095dd3c2145002e33fb2
kyujonglee <kyujong93@naver.com>
2018-12-13 23:11:57 +0900

    css 전면 수정

--- output_with_Django/frontpage/views.py
+++ output_with_Django/frontpage/views.py
@@ -14,7 +14,7 @@ def query(user_input):
                                   "query": {
                                       "multi_match": {
                                           "query": user_input,
-                                          "fields": ["diseaseko^3", "treatment", "symptom^2","synonym^2"],
+                                          "fields": ["diseaseko^3", "treatment", "symptom^2","synonym^0.5"],
                                           "type": "best_fields",
                                           "fuzziness": "auto",
                                           "minimum_should_match": 2
@@ -32,44 +32,62 @@ def query(user_input):
                               size = 100)
     return result
 
-# 검색결과 내 재검색 쿼리
-def re_query(old,new):
-    new_result = es_client.search(index='nnn',
+# 링크 쿼리문
+def link_query(input_link):
+    result = es_client.search(index='nnn',
                               doc_type='disease',
                               body={
                                   "query": {
-                                      "bool": {
-                                          "must": {
-                                              "multi_match": {
-                                                  "query": old,
-                                                  "fields": ["diseaseko^3", "treatment", "symptom^2","synonym^2"],
-                                                  "type": "best_fields",
-                                                  "fuzziness": "auto",
-                                                  "minimum_should_match": 2
-                                              }
-                                          },
-                                          "filter": {
-                                              "match": {
-                                                  "symptom": {
-                                                      "query": new,
+                                      "match": {
+                                          "diseaseko": {
+                                              "query": input_link,
+                                              "fuzziness": "auto",
+                                              "minimum_should_match": 2
+                                          }
+                                      }
+                                  }
+                              },
+                              size = 1)
+    return result
+
+# 검색결과 내 재검색 쿼리
+def re_query(old,new):
+    new_result = es_client.search(index='nnn',
+                                  doc_type='disease',
+                                  body={
+                                      "query": {
+                                          "bool": {
+                                              "must": {
+                                                  "multi_match": {
+                                                      "query": old,
+                                                      "fields": ["diseaseko^3", "treatment", "symptom^2","synonym^0.5"],
+                                                      "type": "best_fields",
                                                       "fuzziness": "auto",
                                                       "minimum_should_match": 2
+                                                  }
+                                              },
+                                              "filter": {
+                                                  "match": {
+                                                      "symptom": {
+                                                          "query": new,
+                                                          "fuzziness": "auto",
+                                                          "minimum_should_match": 2
+                                                      }
                                                   }
                                               }
                                           }
                                       }
-                                  }
-                                  ,
-                                  "highlight": {
-                                      "fragment_size": 2000,
-                                      "number_of_fragments": 0,
-                                      "fields": [
-                                          {"diseaseko": {}},
-                                          {"symptom": {}}
-                                      ]
-                                  }
-                              },
-                              size=100)
+                                      ,
+                                      "highlight": {
+                                          "fragment_size": 2000,
+                                          "number_of_fragments": 0,
+                                          "fields": [
+                                              {"diseaseko": {}},
+                                              {"symptom": {}}
+                                          ]
+                                      }
+                                  },
+                                  size=100)
     return new_result
 
 
