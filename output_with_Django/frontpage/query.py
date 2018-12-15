6d4d81b10bd5695d199bf97f45ca8608c3a0e79b
winty95 <winty95@nate.com>
2018-12-15 02:35:58 +0000

    query code modularization

---
+++ output_with_Django/frontpage/query.py
@@ -0,0 +1,86 @@
+from elasticsearch import Elasticsearch
+es_client = Elasticsearch("localhost:9200")
+
+# 일반 쿼리문
+def query(user_input):
+    result = es_client.search(index='nnn',
+                              doc_type='disease',
+                              body={
+                                  "query": {
+                                      "multi_match": {
+                                          "query": user_input,
+                                          "fields": ["diseaseko^3", "treatment", "symptom^2","synonym^0.5"],
+                                          "type": "best_fields",
+                                          "fuzziness": "auto",
+                                          "minimum_should_match": 2
+                                      }
+                                  },
+                                  "highlight": {
+                                      "fragment_size": 2000,
+                                      "number_of_fragments": 0,
+                                      "fields": [
+                                          {"diseaseko": {}},
+                                          {"symptom": {}}
+                                      ]
+                                  }
+                              },
+                              size = 100)
+    return result
+
+# 링크 쿼리문
+def link_query(input_link):
+    result = es_client.search(index='nnn',
+                              doc_type='disease',
+                              body={
+                                  "query": {
+                                      "match": {
+                                          "diseaseko": {
+                                              "query": input_link,
+                                              "fuzziness": "auto",
+                                              "minimum_should_match": 2
+                                          }
+                                      }
+                                  }
+                              },
+                              size = 1)
+    return result
+
+# 검색결과 내 재검색 쿼리
+def re_query(old,new):
+    new_result = es_client.search(index='nnn',
+                                  doc_type='disease',
+                                  body={
+                                      "query": {
+                                          "bool": {
+                                              "must": {
+                                                  "multi_match": {
+                                                      "query": old,
+                                                      "fields": ["diseaseko^3", "treatment", "symptom^2","synonym^0.5"],
+                                                      "type": "best_fields",
+                                                      "fuzziness": "auto",
+                                                      "minimum_should_match": 2
+                                                  }
+                                              },
+                                              "filter": {
+                                                  "match": {
+                                                      "symptom": {
+                                                          "query": new,
+                                                          "fuzziness": "auto",
+                                                          "minimum_should_match": 2
+                                                      }
+                                                  }
+                                              }
+                                          }
+                                      }
+                                      ,
+                                      "highlight": {
+                                          "fragment_size": 2000,
+                                          "number_of_fragments": 0,
+                                          "fields": [
+                                              {"diseaseko": {}},
+                                              {"symptom": {}}
+                                          ]
+                                      }
+                                  },
+                                  size=100)
+    return new_result
--- output_with_Django/frontpage/views.py
+++ output_with_Django/frontpage/views.py
@@ -3,92 +3,7 @@ from django.shortcuts import redirect
 from django.core import serializers
 from elasticsearch import Elasticsearch
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
-es_client = Elasticsearch("localhost:9200")
-
-# 일반 쿼리문
-def query(user_input):
-    result = es_client.search(index='nnn',
-                              doc_type='disease',
-                              body={
-                                  "query": {
-                                      "multi_match": {
-                                          "query": user_input,
-                                          "fields": ["diseaseko^3", "treatment", "symptom^2","synonym^0.5"],
-                                          "type": "best_fields",
-                                          "fuzziness": "auto",
-                                          "minimum_should_match": 2
-                                      }
-                                  },
-                                  "highlight": {
-                                      "fragment_size": 2000,
-                                      "number_of_fragments": 0,
-                                      "fields": [
-                                          {"diseaseko": {}},
-                                          {"symptom": {}}
-                                      ]
-                                  }
-                              },
-                              size = 100)
-    return result
-
-# 링크 쿼리문
-def link_query(input_link):
-    result = es_client.search(index='nnn',
-                              doc_type='disease',
-                              body={
-                                  "query": {
-                                      "match": {
-                                          "diseaseko": {
-                                              "query": input_link,
-                                              "fuzziness": "auto",
-                                              "minimum_should_match": 2
-                                          }
-                                      }
-                                  }
-                              },
-                              size = 1)
-    return result
-
-# 검색결과 내 재검색 쿼리
-def re_query(old,new):
-    new_result = es_client.search(index='nnn',
-                                  doc_type='disease',
-                                  body={
-                                      "query": {
-                                          "bool": {
-                                              "must": {
-                                                  "multi_match": {
-                                                      "query": old,
-                                                      "fields": ["diseaseko^3", "treatment", "symptom^2","synonym^0.5"],
-                                                      "type": "best_fields",
-                                                      "fuzziness": "auto",
-                                                      "minimum_should_match": 2
-                                                  }
-                                              },
-                                              "filter": {
-                                                  "match": {
-                                                      "symptom": {
-                                                          "query": new,
-                                                          "fuzziness": "auto",
-                                                          "minimum_should_match": 2
-                                                      }
-                                                  }
-                                              }
-                                          }
-                                      }
-                                      ,
-                                      "highlight": {
-                                          "fragment_size": 2000,
-                                          "number_of_fragments": 0,
-                                          "fields": [
-                                              {"diseaseko": {}},
-                                              {"symptom": {}}
-                                          ]
-                                      }
-                                  },
-                                  size=100)
-    return new_result
+from .query import *
 
 
 def pagenation_post(request,result):
@@ -240,3 +155,5 @@ def result(request):
             except:
                 print('no data')
             return render(request, 'frontpage/index.html')
+
+#def result_detail(request):
